1a1cb9c5334414e72c753685db9748b95fbf096d
takumi314 <isospin_symmetry@me.com>
2018-04-14 17:09:15 +0900

    make new ViewController for testing module

---
+++ Sample-UIScrollView/SampleViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SampleViewController.swift
+//  Sample-UIScrollView
+//
+//  Created by NishiokaKohei on 2018/04/14.
+//  Copyright © 2018年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+class SampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
