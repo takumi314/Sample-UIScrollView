b3bf19c0415395d77831ac2f682f0a5171c13402
takumi314 <isospin_symmetry@me.com>
2018-04-14 21:30:37 +0900

    [test] implement it to make sure HorizontalScrollerView class

--- Sample-UIScrollView/SampleViewController.swift
+++ Sample-UIScrollView/SampleViewController.swift
@@ -8,14 +8,41 @@
 
 import UIKit
 
+typealias ImageName = String
+
 class SampleViewController: UIViewController {
+
+    @IBOutlet weak var canvasView: UIView!
+
+    private var scrollerView: HorizontalScrollerView!
+
+    // Images
+    let images: [ImageName] = ["DSC07470", "DSC07471", "DSC07472"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        scrollerView = HorizontalScrollerView(frame: canvasView.bounds)
+        canvasView.addSubview(scrollerView)
+        scrollerView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+}
+
+extension SampleViewController: HorizontalScrollerViewDataSource {
+    func numberOfViewa(in horizontalScrollerView: HorizontalScrollerView) -> Int {
+        return images.count
+    }
+
+    func horizontalScrollerView(_ horizontalScrollerView: HorizontalScrollerView, viewAt index: Int) -> UIView {
+        let image = UIImage(named: images[index])!
+        return UIImageView(image: image)
+    }
 }
